ccbca7c441cd09cc3f23060aaf1de5fbb7c95aa7
Erik Garcia <erik@Eriks-Laptop.home>
2024-08-01 19:18:49 -0400

    Initial commit

--- intervals/script.js
+++ intervals/script.js
@@ -1,5 +1,10 @@
-// number of seconds that have passed
+// Number of seconds that have passed
 let time = 0;
-// reference to the <span> containing the number
+// Reference to the <span> containing the number
 let counterElement = document.getElementById("time");
+function updateCounter() {
+    time++;
+    counterElement.textContent = time;
+}
 
+setInterval(updateCounter, 1000);
